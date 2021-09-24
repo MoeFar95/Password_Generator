67a706c433ce28555ff8682aa3a2852cb584e19d
MoeFar95 <89990024+MoeFar95@users.noreply.github.com>
2021-09-23 21:14:06 -0400

    changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment Code
+
+const generateButton = document.getElementById('generateBtn')
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +12,15 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+
+
+var passwordLength = prompt("You needa enter how many characters my boi");
+
+  var minimumCount = 0;
+  var maximumCount = 12;
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
